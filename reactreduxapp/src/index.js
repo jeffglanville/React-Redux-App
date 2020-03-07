bc185bc7d2b7c56f599a5f7bf1ea335dc54cdcc4
Jeff Glanville <jeff.glanville@outlook.com>
2020-03-07 03:55:09 -0600

    add async data

--- reactreduxapp/src/index.js
+++ reactreduxapp/src/index.js
@@ -6,7 +6,6 @@ import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import { reducer } from './store/reducers/Quotes';
 
 const store = createStore(reducer, applyMiddleware(thunk));
--- reactreduxapp/src/store/reducers/Quotes.js
+++ reactreduxapp/src/store/reducers/Quotes.js
@@ -23,4 +23,4 @@ export const reducer = (state = initialState, action) => {
         default:
             return state;
     }
-}
+};
