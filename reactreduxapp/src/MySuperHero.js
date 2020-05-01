6c7f307bb157f744b3fe58a5a713b7d19bd8073d
Jeff Glanville <jeff.glanville@outlook.com>
2020-04-30 19:57:36 -0500

    update styling

--- reactreduxapp/src/MySuperHero.js
+++ reactreduxapp/src/MySuperHero.js
@@ -7,23 +7,27 @@ import './MySuperHero.css';
 const SuperHero = ({ getSuperHero, SuperHero}) => {
 
     return(
-        <div className="superhero-wrapper">
-            <h1>Super Hero: </h1>
-            {SuperHero.map((hero) => {
-                return (
-                    <div className="superhero">
-                        <p>Name: {hero.name}</p>
-                        <p>Intelligence: {hero.powerstats.intelligence}</p>
-                        <p>Strength: {hero.powerstats.strength}</p>
-                        <p>Speed: {hero.powerstats.speed}</p>
-                        <p>Durability: {hero.powerstats.durability}</p>
-                        <p>Power: {hero.powerstats.power}</p>
-                        <p>Combat: {hero.powerstats.combat}</p>
-                        <p>Race: {hero.appearance.race}</p>
-                    </div>
-                )
-            })}
-            <button onClick={getSuperHero}>Click for a Different Hero</button>
+        <div>
+            <div className="big">
+                <h1>Super Hero: </h1>
+                {SuperHero.map((hero) => {
+                    return (
+                        <div>
+                            <div className="superhero-wrapper">
+                                <p>Name: {hero.name}</p>
+                                <p>Intelligence: {hero.powerstats.intelligence}</p>
+                                <p>Strength: {hero.powerstats.strength}</p>
+                                <p>Speed: {hero.powerstats.speed}</p>
+                                <p>Durability: {hero.powerstats.durability}</p>
+                                <p>Power: {hero.powerstats.power}</p>
+                                <p>Combat: {hero.powerstats.combat}</p>
+                                <p>Race: {hero.appearance.race}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+                </div>
+                <button onClick={getSuperHero}>Click for a Different Hero</button>
         </div>
     );
 };
